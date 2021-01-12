5acdff2c10a2525f31911b131f721591dc61c965
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-01-12 05:19:29 +0100

    feat: Learned about numbers

---
+++ numbers.rb
@@ -0,0 +1,26 @@
+# WORKING WITH NUMBERS
+first_number = 5
+second_number = 3.5
+third_number = -3.1
+
+puts 5 + 9
+puts 5 - 9
+puts 5 * 9
+puts 5 / 9
+puts 2**3 # TWO TO THE POWER OF THREE
+puts 10 % 3
+puts second_number.round()
+puts second_number.ceil()
+puts second_number.floor()
+puts third_number.abs() # ABSOLUTE VALUE OF NUMBER
+
+# MATH CLASS
+puts Math.sqrt(36)
+puts Math.log(1)
+puts Math::PI # PI CONSTANT
+
+# INTEGER WITH DECIMAL WILL RETURN DECIMAL
+puts 10 + 1.00
+
+# HOW TO CONNECT NUMBERS WITH STRINGS - CONVERT
+puts ("My age is " + 20.to_s())
