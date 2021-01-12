336eab94bf1b5cce6f226b19d13d9bed31445f33
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-01-12 09:22:24 +0100

    feat: Learned about for loops

---
+++ notes/for.rb
@@ -0,0 +1,22 @@
+friends = Array[
+  "Jack",
+  "John",
+  "Kevin",
+  "Monica"
+]
+
+for friend in friends
+  puts friend
+end
+
+friends.each do |friend|
+  puts friend
+end
+
+for index in 1..5 
+  puts index
+end
+
+6.times do |index|
+  puts index
+end
