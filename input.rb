447050773c2e26fe18c7604aa03c2ba2311ee3ec
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-01-12 05:38:10 +0100

    feat: Learned about user inputs

---
+++ input.rb
@@ -0,0 +1,9 @@
+puts "Enter your name: "
+name = gets
+puts ("Hello " + name + " , nice to meet you!")
+
+# BY CLICKING ENTER ON INPUT RUBY IS MAKING A NEW LINE
+# TO AVOID WE CAN USE CHOMP
+puts "Enter your age"
+age = gets.chomp()
+puts ("You are " + age + " years old")
