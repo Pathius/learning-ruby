a31de06a2b2dce8a0c45fa2fb8f13f54175686a7
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-01-12 04:56:17 +0100

    feat: Learned about basics

---
+++ basics.rb
@@ -0,0 +1,19 @@
+# COMMENTS ARE MADE WITH HASH
+
+# PRINTING DATA
+puts 'Sup mate'
+print "Hello "
+# PUTS MAKE END OF LINE, SO PRINT WILL BE PRINTED AT ANOTHER LINE
+
+character_name = 'John'
+character_age = '23'
+
+puts "My name is " + character_name
+puts ("I'm " + character_age + " years old")
+
+# DATA TYPES
+car = 'Mike' # STRING
+car_production_year = 2016 # INTEGER NUMBER
+car_price = 30000.99 # FLOATING NUMBER
+car_availability_status = true # BOOLEAN
+car_modifiers = nil # EMPTY VALUE
